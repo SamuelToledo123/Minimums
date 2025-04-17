22c0f635cb1c56eca7b165f4f40a25a1f1e47dc8
KareemKDX <drammeh@msn.com>
2025-04-17 14:30:31 +0200

    CRUD for Ingredients & Tests created

--- src/main/java/se/samuel/minimums/Controller/IngredientsController.java
+++ src/main/java/se/samuel/minimums/Controller/IngredientsController.java
@@ -5,20 +5,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.samuel.minimums.Dto.IngredientsDto;
 import se.samuel.minimums.Models.Ingredients;
 import se.samuel.minimums.Service.IngredientsService;
 
+/*
 @RestController
 @RequestMapping("/api/ingredients")
 public class IngredientsController {
 
 
-    @Autowired
+@Autowired
     IngredientsService ingredientsService;
     @PostMapping
-    public Ingredients create(@RequestBody Ingredients ingredient) {
+    public Ingredients create(@RequestBody IngredientsDto ingredient) {
         return ingredientsService.createIngredient(ingredient);
     }
 
 
 }
+
+ */
+
+
